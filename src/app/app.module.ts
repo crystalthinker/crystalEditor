ed46c255da5d286beb3dc46ffce2a16846840f3c
abhilashkr-qb <abhilash@qburst.com>
2018-03-12 16:39:55 +0530

    crystal editor added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
+import {ReactiveFormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot(),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
